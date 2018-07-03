4f5104452db55a48bceeaf4dd019f50828ed1caf
wqr <wqrCoding@163.com>
2018-07-03 21:51:29 +0800

    Complete makeGrid()

--- designs.js
+++ designs.js
@@ -5,10 +5,27 @@ const gridHeight = document.getElementById('inputHeight').value;
 const gridWidth = document.getElementById('inputWidth').value ;
 // When size is submitted by the user, call makeGrid()
 
+
 function makeGrid() {
     let i;
     let j;
+    const table = document.getElementById('pixelCanvas');
     for(i=0;i<gridHeight;i++) {
-        
+        addTr(table);
+        for(j=0;j<gridWidth;j++) {
+            addTh();
+        }
     }
+}
+
+//增加表格行
+function addTr(table){
+    const newTr = document.createElement("tr"); 
+    table.appendChild(newTr); 
+}
+
+//增加表格列
+function addTr(table){
+    const newTh = document.createElement("th"); 
+    tr.appendChild(newTh); 
 }
