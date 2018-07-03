69c7d2d07c62e103c2102744dfd295998ecc61b5
wqr <wqrCoding@163.com>
2018-07-03 12:14:18 +0800

    Const color,grideHeight,grideWidth

--- designs.js
+++ designs.js
@@ -1,6 +1,8 @@
 // Select color input
+const color = document.getElementById('#colorPicker');
 // Select size input
-
+const gridHeight = document.getElementById('#inputHeight');
+const gridWidth = document.getElementById('#inputWidth');
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
